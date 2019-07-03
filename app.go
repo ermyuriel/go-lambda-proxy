01a916b6c18c4c4f6ce7972b91f08266bede344c
ermyuriel@gmail.com <ermyuriel@gmail.com>
2019-07-03 17:54:47 -0500

    comments

--- app.go
+++ app.go
@@ -10,7 +10,7 @@ import (
 
 type contextKey string
 
-//Executor is an alias for func(context.Context) (interface{}, error)
+//Executor is an alias for func(context.Context) (interface{}, error). Inside the function, you will find a context with an events.APIGatewayProxyRequest value identified with key "request"
 type Executor func(context.Context) (interface{}, error)
 
 //ProxyFunction receives an executor and returns an APIGatewayProxyResponse with json marshalled body and error return
@@ -26,6 +26,7 @@ func ProxyFunction(f Executor) func(request events.APIGatewayProxyRequest) (even
 		if err == nil {
 			statusResponse = http.StatusOK
 		}
+
 		encodedResult, encodingErr := json.Marshal(result)
 		if err != nil {
 			return events.APIGatewayProxyResponse{
@@ -34,6 +35,7 @@ func ProxyFunction(f Executor) func(request events.APIGatewayProxyRequest) (even
 			}, encodingErr
 
 		}
+
 		response := events.APIGatewayProxyResponse{
 			StatusCode: statusResponse,
 			Body:       string(encodedResult),
